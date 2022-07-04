510d1dcdb9faa61f5038f2ab7d0cc4a30c22b21e
billypharel <ngahanana2000@gmail.com>
2022-07-03 18:32:20 -0700

    my task

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    si idx < 0 :
-        retour (Aucun)
-
-    longueur = len(my_list)
-
-    si idx > longueur - 1:
-        retour (Aucun)
-
-    retour(my_list[idx])
+    for arr in range(len(my_list)):
+        if ((idx < 0) or (idx > len(my_list))):
+            return (None)
+        elif (idx == arr):
+            return (my_list[arr])
