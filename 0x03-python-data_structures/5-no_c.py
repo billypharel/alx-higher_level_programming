f5df90cf7e2560945accbc77339222675a88326d
billypharel <ngahanana2000@gmail.com>
2022-07-03 19:00:31 -0700

    my task

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    for i in range(len(my_string) - 2):
-        if (my_string[i] == 'c' or my_string[i] == 'C'):
-            my_string = my_string[:i] + "" + my_string[i+1:]
-    return (my_string)
+    longueur = len(my_string)
+
+    j = 0
+
+    new_string = my_string[:]
+
+    pour i en plage(longueur):
+        si (my_string[i] == 'c' ou my_string[i] == 'C'):
+            new_string = new_string[:(i - j)] + my_string[(i + 1):]
+            j += 1
+
+    retour (new_string)
