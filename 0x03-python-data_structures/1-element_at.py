8cc30a9c8da381eaa505ee373d26dfbe3060c20f
billypharel <ngahanana2000@gmail.com>
2022-07-03 18:25:45 -0700

    my task

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    si idx < 0 :
+        retour (Aucun)
+
+    longueur = len(my_list)
+
+    si idx > longueur - 1:
+        retour (Aucun)
+
+    retour(my_list[idx])
