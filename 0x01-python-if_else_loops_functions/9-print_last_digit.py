bbbc300bb0b5918aec6b9142d55da8aa8d1b1b3c
billypharel <ngahanana2000@gmail.com>
2022-07-03 17:02:28 -0700

    my task

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-def uppercase(str):
-    for c in range(len(str)):
-        n = ord(str[c])
-        if (n >= 97 and n <= 122):
-            n -= 32
-        print("{}".format(chr(n)), end="")
-    print()
+def print_last_digit(number):
+    if (number < 0):
+        number *= -1
+    ldigit = number % 10
+    print("{:d}".format(ldigit), end="")
+    return ldigit
