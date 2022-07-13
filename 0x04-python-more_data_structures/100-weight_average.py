15ceb2d06d21c323c1cdeae324e204c83eb17291
billypharel <ngahanana2000@gmail.com>
2022-07-13 08:32:34 -0700

    my task

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    if my_list == [] or my_list is None:
+    if not my_list:
         return 0
-    sum_t = total = 0
-    for (x, y) in my_list:
-        sum_t += (x * y)
-        total += y
-    return sum_t / 
+
+    num = 0
+    den = 0
+
+    for tup in my_list:
+        num += tup[0] * tup[1]
+        den += tup[1]
+
+    return (num / den)
